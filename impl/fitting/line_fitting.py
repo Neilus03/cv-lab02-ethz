75818518e643df23e9c288a1cfc81fed7aac70d9
Neil de la fuente <87651732+Neilus03@users.noreply.github.com>
2025-10-15 20:15:08 +0200

    updated

--- impl/fitting/line_fitting.py
+++ impl/fitting/line_fitting.py
@@ -6,27 +6,75 @@ np.random.seed(0)
 random.seed(0)
 
 def least_square(x,y):
+
 	# TODO
 	# return the least-squares solution
 	# you can use np.linalg.lstsq
-	return k, b
+
+	A = np.vstack([x, np.ones_like(x)]).T #[x,1]
+	(k,b), *_ = np.linalg.lstsq(A,y,rcond=None)
+
+	return k, b #k=slope, b=intercept
 
 def num_inlier(x,y,k,b,n_samples,thres_dist):
 	# TODO
 	# compute the number of inliers and a mask that denotes the indices of inliers
-	num = 0
-	mask = np.zeros(x.shape, dtype=bool)
+
+	# Distance from each point to the line y = kx + b:
+	# |k*x - y + b| / sqrt(k^2 + 1)
+
+	denom = np.sqrt(k**2 +1)
+	d = np.abs(k*x - y + b) / denom
+
+	mask = d < thres_dist
+	num = int(np.count_nonzero(mask))
 
 	return num, mask
 
 def ransac(x,y,iter,n_samples,thres_dist,num_subset):
+	# RANSAC for y = kx + b
 	# TODO
 	# ransac
 	k_ransac = None
 	b_ransac = None
-	inlier_mask = None
+	inlier_mask = np.zeros(n_samples, dtype=bool)
 	best_inliers = 0
 
+
+	#minimal subset size (2 points for a line)
+	m = max(2, int(num_subset))
+
+	for _ in range(iter):
+		#get random minimal subset
+		idx = np.random.choice(n_samples, size=m, replace=False)
+		#provisional fit on the subset with least squares
+		A = np.c_[x[idx], np.ones(m)]
+		k_hat, b_hat = np.linalg.lstsq(A, y[idx], rcond=None)[0]
+
+		#get score by counting inliers:
+		num, mask = num_inlier(x, y, float(k_hat), float(b_hat), n_samples, thres_dist)
+
+		#keep the best so far:
+		if num > best_inliers:
+			best_inliers = num
+			inlier_mask = mask
+			k_ransac = float(k_hat)
+			b_ransac = float(b_hat)
+
+
+
+	if best_inliers > 0:
+		A = np.c_[x[inlier_mask], np.ones(best_inliers)]
+		k_ref, b_ref = np.linalg.lstsq(A, y[inlier_mask], rcond=None)[0]
+		k_ransac = float(k_ref)
+		b_ransac = float(b_ref)
+	else:
+		A = np.c_[x, np.ones(n_samples)]
+		k_all, b_all = np.linalg.lstsq(A, y, rcond=None)[0]
+		k_ransac = float(k_all)
+		b_ransac = float(b_all)
+		inlier_mask = np.ones(n_samples, dtype=bool)
+        
 	return k_ransac, b_ransac, inlier_mask
 
 def main():
